3eac742a83dafa46433853aa8b5d888ca3f0b1b8
liu-muwen <2364513306@qq.com>
2026-03-07 11:52:18 +0800

    引入rocketmq异步流量统计

--- project/src/main/java/com/nageoffer/shortlink/project/mq/producer/ShortLinkStatsSaveProducer.java
+++ project/src/main/java/com/nageoffer/shortlink/project/mq/producer/ShortLinkStatsSaveProducer.java
@@ -3,36 +3,48 @@ package com.nageoffer.shortlink.project.mq.producer;
 import com.alibaba.fastjson2.JSON;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.MessageConst;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+/**
+ * 短链接监控状态保存消息队列生产者
+ * 公众号：马丁玩编程，回复：加群，添加马哥微信（备注：link）获取项目资料
+ */
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class ShortLinkStatsSaveProducer {
 
-    private final RabbitTemplate rabbitTemplate;
+    private final RocketMQTemplate rocketMQTemplate;
 
-    @Value("${rabbitmq.exchange.stats-save:short-link.stats.exchange}")
-    private String statsSaveExchange;
+    @Value("${rocketmq.producer.topic}")
+    private String statsSaveTopic;
 
-    @Value("${rabbitmq.routing-key.stats-save:short-link.stats.routing-key}")
-    private String statsSaveRoutingKey;
-
+    /**
+     * 发送延迟消费短链接统计
+     */
     public void send(Map<String, String> producerMap) {
         String keys = UUID.randomUUID().toString();
         producerMap.put("keys", keys);
+        Message<Map<String, String>> build = MessageBuilder
+                .withPayload(producerMap)
+                .setHeader(MessageConst.PROPERTY_KEYS, keys)
+                .build();
+        SendResult sendResult;
         try {
-            // 直接发送，RabbitMQ 会根据 Exchange 和 RoutingKey 投递
-            rabbitTemplate.convertAndSend(statsSaveExchange, statsSaveRoutingKey, producerMap);
-            log.info("[消息访问统计监控] 消息发送成功，消息Keys：{}", keys);
+            sendResult = rocketMQTemplate.syncSend(statsSaveTopic, build, 2000L);
+            log.info("[消息访问统计监控] 消息发送结果：{}，消息ID：{}，消息Keys：{}", sendResult.getSendStatus(), sendResult.getMsgId(), keys);
         } catch (Throwable ex) {
             log.error("[消息访问统计监控] 消息发送失败，消息体：{}", JSON.toJSONString(producerMap), ex);
+            // 自定义行为...
         }
     }
 }
